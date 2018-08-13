5b476f12c4cf3a7ec4487ad4674d6cb1acbd4b8b
Steven Xie <dev@stevenxie.me>
2018-08-12 22:32:08 -0400

    Implemented io.ReaderFrom for portal.Portal

--- portal/portal.go
+++ portal/portal.go
@@ -76,6 +76,30 @@ func (p *Portal) Read(dst []byte) (n int, err error) {
 	return n, err
 }
 
+// ReadFrom allows for the piping of data from a io.Writer into a command's
+// standard output.
+func (p *Portal) ReadFrom(r io.Reader) (n int64, err error) {
+	in, err := p.StdinPipe()
+	if err != nil {
+		return 0, err
+	}
+
+	if err = p.Start(); err != nil {
+		return 0, err
+	}
+
+	if n, err = io.Copy(in, r); err != nil {
+		return n, fmt.Errorf("failed to write to %s command input: %v", p.Name, err)
+	}
+
+	if err = in.Close(); err != nil {
+		return n, fmt.Errorf("failed to close input to %s command: %v", p.Name, err)
+	}
+
+	err = p.Wait()
+	return n, err
+}
+
 // Write allows for the writing of data into a command's standard input.
 func (p *Portal) Write(src []byte) (n int, err error) {
 	in, err := p.StdinPipe()
